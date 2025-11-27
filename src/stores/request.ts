da0ba4ba6cb914220426045ce805cecc522787b0
kyssion <1409915687@qq.com>
2025-11-28 03:26:12 +0800

    f

--- src/stores/request.ts
+++ src/stores/request.ts
@@ -80,7 +80,7 @@ export function useAPIInfo () {
     const qs = new URLSearchParams(location.search)
 
     const hostname = qs.get('host') ?? hostsStorage?.[hostSelectIdxStorage]?.hostname ?? url?.hostname ?? '127.0.0.1'
-    const port = qs.get('port') ?? hostsStorage?.[hostSelectIdxStorage]?.port ?? url?.port ?? '9090'
+    const port = qs.get('port') ?? hostsStorage?.[hostSelectIdxStorage]?.port ?? url?.port ?? '9091'
     const secret = qs.get('secret') ?? hostsStorage?.[hostSelectIdxStorage]?.secret ?? url?.username ?? ''
     const protocol = qs.get('protocol') ?? hostname === '127.0.0.1' ? 'http:' : (url?.protocol ?? window.location.protocol)
 
